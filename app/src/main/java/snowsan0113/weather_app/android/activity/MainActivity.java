a30aeb976aa7d7d7c6ce970334365272dff701b1
snowsan0113 <egawayuuki0113@gmail.com>
2025-11-04 21:15:39 +0900

    【追加】新規追加

---
+++ app/src/main/java/snowsan0113/weather_app/android/activity/MainActivity.java
@@ -0,0 +1,35 @@
+package snowsan0113.weather_app.android.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import snowsan0113.weather_app.android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_activity_root), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initApp();
+    }
+
+    private void initApp() {
+        Intent home_intent = new Intent(this, WeatherHomeActivity.class);
+        home_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(home_intent);
+    }
+}
---
+++ app/src/main/java/snowsan0113/weather_app/android/activity/WeatherHomeActivity.java
@@ -0,0 +1,26 @@
+package snowsan0113.weather_app.android.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import snowsan0113.weather_app.android.R;
+
+public class WeatherHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_weather_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home_activity_root), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
