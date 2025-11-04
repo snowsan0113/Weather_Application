b6e64486172a3528f4f59204c31adbdca9ca4842
snowsan0113 <egawayuuki0113@gmail.com>
2025-11-04 23:04:53 +0900

    【追加】ホーム画面に、時間のレイアウトを追加

---
+++ app/src/main/java/snowsan0113/weather_app/android/fragment/WeatherFragment.java
@@ -0,0 +1,33 @@
+package snowsan0113.weather_app.android.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import snowsan0113.weather_app.android.R;
+
+public class WeatherFragment extends Fragment {
+
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
+
+    public static WeatherFragment newInstance() {
+        return new WeatherFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
